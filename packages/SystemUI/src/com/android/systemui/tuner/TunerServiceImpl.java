abf3bb0048d3e306064c1d06a24a5118b0ccb331
maxwen <max.weninger@gmail.com>
2018-08-17 16:04:35 +0200

    base: SystemUI: enable tuner and show in BlazeHouse
    Change-Id: I4d3ff3ca989e6a6952419d147208318ee269b9d7
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -126,6 +126,7 @@ public class TunerServiceImpl extends TunerService {
         };
         mUserTracker.addCallback(mCurrentUserTracker,
                 new HandlerExecutor(mainHandler));
+        setTunerEnabled(true);
     }
 
     @Override
