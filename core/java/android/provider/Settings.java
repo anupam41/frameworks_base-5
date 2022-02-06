61d6bb468c921415099e48daf955b2e7f808111e
Alex Cruz <du.alexcruz@gmail.com>
2022-02-06 16:51:19 +0000

    Bring back traffic indicator for all!
    arrow-edits:
     - adapt to Q
    
    Change-Id: Ia41474a262b258f8a2d4d7b10a15891729ffd17c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5557,6 +5557,18 @@ public final class Settings {
         public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+	/**
          * Whether to force small clock on keyguard
          * @hide
          */
