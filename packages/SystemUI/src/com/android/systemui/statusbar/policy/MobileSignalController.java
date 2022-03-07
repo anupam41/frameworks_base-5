AOSPA/android_frameworks_base-old

SystemUI: support VoWIFI icons
Show VoWIFI icon when VoWIFI is available
Show VoWIFI call icon when voice call is over WIFI

CRs-Fixed: 2689484
Change-Id: Id5b8ea61d8dbf129f17bd6e9cd0f39b4bc440506

packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -492,6 +492,14 @@ public class MobileSignalController extends SignalController<
         if ( mConfig.enableRatIconEnhancement ) {
             typeIcon = getEnhancementDataRatIcon();
         }
+
+        MobileIconGroup vowifiIconGroup = getVowifiIconGroup();
+        if ( mConfig.showVowifiIcon && vowifiIconGroup != null ) {
+            typeIcon = vowifiIconGroup.mDataType;
+            statusIcon = new IconState(true,
+                    mCurrentState.enabled && !mCurrentState.airplaneMode? statusIcon.icon : 0,
+                    statusIcon.contentDescription);
+        }
         if (DEBUG) {
             Log.d(mTag, "notifyListeners mConfig.alwaysShowNetworkTypeIcon="
                     + mConfig.alwaysShowNetworkTypeIcon + "  mDataNetType:" + mDataNetType +
@@ -503,7 +511,8 @@ public class MobileSignalController extends SignalController<
                     + " icons.mDataType=" + icons.mDataType
                     + " mConfig.showVolteIcon=" + mConfig.showVolteIcon
                     + " isVolteSwitchOn=" + isVolteSwitchOn()
-                    + " volteIcon=" + volteIcon);
+                    + " volteIcon=" + volteIcon
+                    + " mConfig.showVowifiIcon=" + mConfig.showVowifiIcon);
         }
         callback.setMobileDataIndicators(statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 activityIn, activityOut, volteIcon, dataContentDescription, dataContentDescriptionHtml,
@@ -1034,6 +1043,21 @@ public class MobileSignalController extends SignalController<
             ratIcon = iconGroup.mDataType;
         }
         return ratIcon;
+    }
+
+    private boolean isVowifiAvailable() {
+        return mCurrentState.voiceCapable &&  mCurrentState.imsRegistered
+                && getDataNetworkType() == TelephonyManager.NETWORK_TYPE_IWLAN;
+    }
+
+    private MobileIconGroup getVowifiIconGroup() {
+        if ( isVowifiAvailable() && !isCallIdle() ) {
+            return TelephonyIcons.VOWIFI_CALLING;
+        }else if (isVowifiAvailable()) {
+            return TelephonyIcons.VOWIFI;
+        }else {
+            return null;
+        }
     }
 
     @Override

anupam41/frameworks_base-5

Signed-off-by: DennySPb <dennyspb@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -503,10 +503,6 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 case 4:
                     resId = R.drawable.ic_volte3;
                     break;
-                // CAF HD Icon
-                case 5:
-                    resId = R.drawable.ic_hd2_volte;
-                    break;
                 // MIUI 11 VoLTE icon
                 case 6:
                     resId = R.drawable.ic_volte_miui;
@@ -674,7 +670,15 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                     && mCurrentState.activityOut;
             showDataIcon &= mCurrentState.isDefault || dataDisabled;
             int typeIcon = (showDataIcon || mConfig.alwaysShowDataRatIcon) ? icons.dataType : 0;
+            MobileIconGroup vowifiIconGroup = getVowifiIconGroup();
+            if (vowifiIconGroup != null ) {
+                typeIcon = vowifiIconGroup.dataType;
+                statusIcon = new IconState(true,
+                        mCurrentState.enabled && !mCurrentState.airplaneMode? statusIcon.icon : -1,
+                        statusIcon.contentDescription);
+            }
             boolean showTriangle = mCurrentState.enabled && !mCurrentState.airplaneMode;
+
             MobileDataIndicators mobileDataIndicators = new MobileDataIndicators(
                     statusIcon, qsIcon, typeIcon, qsTypeIcon,
                     activityIn, activityOut, dataContentDescription, dataContentDescriptionHtml,
@@ -1049,6 +1053,30 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     @VisibleForTesting
     void setImsType(int imsType) {
         mImsType = imsType;
+    }
+
+    private boolean isCallIdle() {
+        return mCallState == TelephonyManager.CALL_STATE_IDLE;
+    }
+
+    private int getDataNetworkType() {
+        return mServiceState != null ?
+                mServiceState.getDataNetworkType() : TelephonyManager.NETWORK_TYPE_UNKNOWN;
+    }
+
+    private boolean isVowifiAvailable() {
+        return mCurrentState.voiceCapable &&  mCurrentState.imsRegistered
+                && getDataNetworkType() == TelephonyManager.NETWORK_TYPE_IWLAN;
+    }
+
+    private MobileIconGroup getVowifiIconGroup() {
+        if ( isVowifiAvailable() && !isCallIdle() ) {
+            return TelephonyIcons.VOWIFI_CALLING;
+        }else if (isVowifiAvailable()) {
+            return TelephonyIcons.VOWIFI;
+        }else {
+            return null;
+        }
     }
 
     @Override